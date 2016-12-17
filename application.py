085dc54aca5a09f685b0ebcbcbe19e8f758c1bfc
jaggubhai <sankurathri6@gmail.com>
2016-12-17 17:08:45 +0000

    modified application.py

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 from flask import Flask
-#from user.views import user_app
+from flask.ext.mongoengine import MongoEngine #adding mongo engine
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+   
+    db.init_app(app) # creating the database within the application
 
     from user.views import user_app
     app.register_blueprint(user_app)   
