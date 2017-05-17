a10568ab4cfa723e85aaee2fc684d2d9299cbf70
jaggubhai <sankurathri6@gmail.com>
2017-05-17 10:24:16 +0000

    updated forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import Form
 from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
+from wtforms.validators import ValidationError
+
+
 
 class RegisterForm(Form):
     first_name = StringField('First name', [validators.Required()]) #if these fields are empty it will ask user to mention them
