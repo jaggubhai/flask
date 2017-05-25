9ef03a2e7ece37efb3b9675a552682e2de119a76
jaggubhai <sankurathri6@gmail.com>
2017-05-25 12:26:27 +0000

    validators are addeed

--- user/forms.py
+++ user/forms.py
@@ -4,7 +4,7 @@ from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
 
 
-
+from user.models import User
 class RegisterForm(Form):
     first_name = StringField('First name', [validators.Required()]) #if these fields are empty it will ask user to mention them
     last_name = StringField('Last name', [validators.Required()])
@@ -22,6 +22,14 @@ class RegisterForm(Form):
         validators.length(min=8, max=15)
         ])
     confirm = PasswordField('Repeat Password')
+
+    def validate_username(form, field):
+	if User.objects.filter(username=field.data).first():
+	    raise ValidationError("Username already exists")
+
+    def validate_email(form, field):
+	if User.objects.filter(email=field.data).first():
+	    raise ValidationError("Email is already in use")
 
 
 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
-from forms import RegisterForm
+import bcrypt
+
+from user.models import User
+from user.forms import RegisterForm
 user_app = Blueprint('user_app', __name__)
 
 @user_app.route('/login')
@@ -9,6 +12,18 @@ def login():
 @user_app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+	salt = bcrypt.gensalt()
+	hashed_password = bcrypt.hashpw(form.password.data, salt)
+	user = User(
+	       username=form.username.data,
+	       password=hashed_password,
+	       email=form.email.data,
+	       first_name=form.first_name.data,
+	       last_name=form.last_name.data,
+	       )
+	user.save()
+	return "User registered"
     return render_template('user/register.html', form=form)
 
 
